d7e66da672206bac752d9920cfd6a5cd8ff0f481
EleonoraMassiglia <167798814+EleonoraMassiglia@users.noreply.github.com>
2024-05-15 10:06:42 +0200

    Add files via upload

---
+++ test.js
@@ -0,0 +1,28 @@
+// DOMANDA 1:
+// Cos'è un algoritmo? Prova a spiegarlo a parole tue, come se lo dovessi spiegare a qualcuno che non
+// capisce niente di informatica.
+
+// RISPOSTA 1:
+// È un pezzo di codice che esegue qualcosa che serve per l’app. Dopo che l’utente inserisce gli input l’algoritmo
+// svolge la sua funzione. Ad esempio sommare numero A e numero B quando questi due vengono definiti dall’utente.
+// I numeri non sono predefiniti (utente inserisce numero A = 5 e numero B = 3 l’algoritmo darà come risultato 8)
+
+
+
+// DOMANDA 2:
+// Cos'è una variabile? Prova a spiegarlo a parole tue, come se lo dovessi spiegare a qualcuno che non
+// capisce niente di informatica.
+
+// RISPOSTA 2:
+// Le variabili possono essere viste come dei "contenitori" dove vengono inserite le informazioni che, in seguito
+// si dovranno richiamare o utilizzare all'interno del codice.
+
+
+// DOMANDA 3:
+// undefined e null sono la stessa cosa?
+
+// RISPOSTA 3:
+// Undefined e null non sono la stessa cosa.
+// Undefined si utilizza quando la variabile non è stata dichiarata o, anche se dichiarata, non gli è stato
+// assegnato un valore specifico.
+// Null è un assegnazione che significa nessun valore
